ff1ea8d0a6b69fa56af71a8dcee94b091540430e
Julian David Triana Roa ⚡ <74771189+juliandtrianar@users.noreply.github.com>
2024-09-04 20:45:38 -0500

    Update Immortal.java

--- LABS/LAB3/src/main/java/edu/eci/arsw/highlandersim/Immortal.java
+++ LABS/LAB3/src/main/java/edu/eci/arsw/highlandersim/Immortal.java
@@ -2,13 +2,15 @@ package edu.eci.arsw.highlandersim;
 
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Immortal extends Thread {
 
-    private ImmortalUpdateReportCallback updateCallback=null;
-    
+    private ImmortalUpdateReportCallback updateCallback = null;
+
     private int health;
-    
+
     private int defaultDamageValue;
 
     private final List<Immortal> immortalsPopulation;
@@ -17,26 +19,38 @@ public class Immortal extends Thread {
 
     private final Random r = new Random(System.currentTimeMillis());
 
+    private boolean execution;
 
-    public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue, ImmortalUpdateReportCallback ucb) {
+    public Immortal(String name, List<Immortal> immortalsPopulation, int health, int defaultDamageValue,
+            ImmortalUpdateReportCallback ucb) {
         super(name);
-        this.updateCallback=ucb;
+        this.updateCallback = ucb;
         this.name = name;
         this.immortalsPopulation = immortalsPopulation;
         this.health = health;
-        this.defaultDamageValue=defaultDamageValue;
+        this.defaultDamageValue = defaultDamageValue;
+        this.execution = true;
     }
 
     public void run() {
 
-        while (true) {
+        while (this.getHealth() > 0 && immortalsPopulation.size() > 1) {
+            synchronized (immortalsPopulation) {
+                while (!execution) {
+                    try {
+                        immortalsPopulation.wait();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Immortal.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
             Immortal im;
 
             int myIndex = immortalsPopulation.indexOf(this);
 
             int nextFighterIndex = r.nextInt(immortalsPopulation.size());
 
-            //avoid self-fight
+            // avoid self-fight
             if (nextFighterIndex == myIndex) {
                 nextFighterIndex = ((nextFighterIndex + 1) % immortalsPopulation.size());
             }
@@ -44,6 +58,9 @@ public class Immortal extends Thread {
             im = immortalsPopulation.get(nextFighterIndex);
 
             this.fight(im);
+            if (im.getHealth() <= 0) {
+                immortalsPopulation.remove(im);
+            }
 
             try {
                 Thread.sleep(1);
@@ -56,15 +73,35 @@ public class Immortal extends Thread {
     }
 
     public void fight(Immortal i2) {
-
-        if (i2.getHealth() > 0) {
-            i2.changeHealth(i2.getHealth() - defaultDamageValue);
-            this.health += defaultDamageValue;
-            updateCallback.processReport("Fight: " + this + " vs " + i2+"\n");
-        } else {
-            updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+        Immortal firstLock = minImmortalHash(i2);
+        Immortal secondLock = firstLock == this ? i2 : this;
+        synchronized (firstLock) {
+            synchronized (secondLock) {
+                if (this.getHealth() > 0) {
+                    if (i2.getHealth() > 0) {
+                        i2.changeHealth(i2.getHealth() - defaultDamageValue);
+                        this.health += defaultDamageValue;
+                        updateCallback.processReport("Fight: " + this + " vs " + i2 + "\n");
+                    } else {
+                        updateCallback.processReport(this + " says:" + i2 + " is already dead!\n");
+                    }
+                }
+            }
         }
-
+    }
+    
+    public Immortal minImmortalHash(Immortal i2) {
+        Immortal minImmortal;
+        int immortalOneHash = System.identityHashCode(this);
+        int immortalTwoHash = System.identityHashCode(i2);
+        if (immortalOneHash < immortalTwoHash) {
+            minImmortal = this;
+        } else if (immortalOneHash > immortalTwoHash) {
+            minImmortal = i2;
+        } else {
+            minImmortal = this;
+        }
+        return minImmortal;
     }
 
     public void changeHealth(int v) {
@@ -75,6 +112,10 @@ public class Immortal extends Thread {
         return health;
     }
 
+    public void setExecution(boolean execution) {
+        this.execution = execution;
+    }
+
     @Override
     public String toString() {
 
@@ -82,3 +123,4 @@ public class Immortal extends Thread {
     }
 
 }
+
